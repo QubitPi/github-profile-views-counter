c348229fe40dcf20f814de1a67a2460a021becf3
Jack <jack20220723@gmail.com>
2024-12-23 11:55:20 +0800

    Add triggering-based config.json updator

--- update_config_json.py
+++ update_config_json.py
@@ -1,9 +1,21 @@
 import json
 import requests
 
+URL_TEMPLATE = "https://api.github.com/users/QubitPi/repos?page={page}&per_page={per_page}"
+
 def main():
-    all_public_repos = requests.get(url="https://api.github.com/users/QubitPi/repos").json()
-    names = [repo["name"] for repo in all_public_repos]
+    page = 1
+    per_page = 30
+
+    names = []
+
+    while True:
+        paged = [repo["name"] for repo in requests.get(url=URL_TEMPLATE.format(page=page, per_page=per_page)).json()]
+        if len(paged) > 0:
+            names += paged
+            page += 1
+        else:
+            break
 
     f = open("config.json", "w")
     f.write(json.dumps({
